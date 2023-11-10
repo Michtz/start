476899785120490102ec3b9e2557c9df12c8c9ae
Michtz <105941997+Michtz@users.noreply.github.com>
2023-11-10 18:40:47 +0100

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {
-  BueroHorw,
-  PuristischesBadezimmer,
-  BueroVideolution,
-  StudioNeubau,
-  WohnungAmSee
-} from './pages/Projects';
 import Home from './pages/Home';
-import { Datenschutz, Impressum } from './pages/Legal';
 import { LanguageProvider } from './CreateContent/LanguageProvider';
-import Error from './pages/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -21,14 +12,6 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/OfficeHorw" element={<BueroHorw />} />
-          <Route path="/PuristicBathroom" element={<PuristischesBadezimmer />} />
-          <Route path="/NewConstructionStudio" element={<StudioNeubau />} />
-          <Route path="/OfficeVideolution" element={<BueroVideolution />} />
-          <Route path="/LakefrontApartment" element={<WohnungAmSee />} />
-          <Route path="/Imprint" element={<Impressum />} />
-          <Route path="/Privacy" element={<Datenschutz />} />
-          <Route path="/*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </LanguageProvider>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,4 @@
-import AboutContainer from '../component/container/home/AboutContainer';
-import DreieckComponent from '../component/system/DreieckComponent';
-import Footer from '../component/container/FooterContainer';
-import HomeHeaderContainer from '../component/container/home/HomeHeaderContainer';
-import OfferContainer from '../component/container/home/OfferContainer';
-import ProjectContainer from '../component/container/home/ProjectContainer';
-import LocationContainer from '../component/container/home/LocationContainer';
-import CustomersAndClients from '../component/container/home/CustomersAndClientsContainer';
-import { ScrolbarRemover } from '../component/functions/function';
-import LoadingOverlay from '../component/functions/onLoadFunction';
-
 const Home = () => {
-  ScrolbarRemover();
-  return (
-    <>
-      <LoadingOverlay />
-      <HomeHeaderContainer />
-      <OfferContainer />
-      <DreieckComponent />
-      <ProjectContainer />
-      <DreieckComponent />
-      <AboutContainer />
-      <DreieckComponent />
-      <CustomersAndClients />
-      <DreieckComponent />
-      <LocationContainer />
-      <Footer />
-    </>
-  );
+  return <>hallo world </>;
 };
 export default Home;
